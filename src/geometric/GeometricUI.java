078412b1fd9c82139f6da40c3be8c796dc15aa24
Sanne <sanne.vanhegelsom@ru.nl>
2022-02-19 19:42:16 +0100

    GemometricUI not finished

--- src/geometric/GeometricUI.java
+++ src/geometric/GeometricUI.java
@@ -1,35 +1,90 @@
 package geometric;
 
+import java.util.Scanner;
+
 public class GeometricUI {
-	public GeometricUI() {
-		startProgram();
-	}
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    int number = scanner.nextInt();
 
-	private void startProgram() {
+    public GeometricUI() {
+        startProgram();
+        quit();
+        showGeometrics();
+        circle();
+        rectangle();
+        move();
+        remove();
+        sort();
+    }
 
-	}
+    private void startProgram() {
+        System.out.println("Choose one of these commands (quit, show, circle, rectangle, move, remove, sort: ");
+        switch (input) {
+            case "quit":
+                quit();
+            case "show":
+                showGeometrics();
+            case "circle":
+                circle();
+            case "rectangle":
+                rectangle();
+            case "move":
+                move();
+            case "remove":
+                remove();
+            case "sort":
+                sort();
+        }
+    }
 
-	private void showGeometrics(){
+    //break?
+    private void quit() {
 
-	}
+    }
 
-	private void circle (){
+    //show the whole array with the geometric objects
+    private void showGeometrics() {
 
-	}
+    }
 
-	private void rectacle (){
+    //put object in array
+    private void circle() {
+        System.out.println("Give the x coordinate of the centre: ");
+        int x = number;
+        System.out.println("Give the y coordinate of the centre: ");
+        int y = number;
+        System.out.println("Give the radius of the circle: ");
+        int r = number;
+    }
 
-	}
+    //put object in arrau
+    private void rectangle() {
+        System.out.println("Give the width of the rectangle: ");
+        int width = number;
+        System.out.println("Give the height of the rectangle: ");
+        int height = number;
+        System.out.println("Give the x coordinate of the left corner of the rectangle: ");
+        int x = number;
+        System.out.println("Give the y coordinate of the left corner of the rectangle: ");
+        int y = number;
+    }
 
-	private void move (){
+    //move object i in the array with dx and dy
+    private void move() {
+        System.out.println("How many steps you want to move the object horizontal? ");
+        int dx = number;
+        System.out.println("How many steps you want to move the object vertical? ");
+        int dy = number;
+    }
 
-	}
+    //remove object i in the array (.remove?)
+    private void remove() {
 
-	private void remove (){
+    }
 
-	}
+    //(?)
+    private void sort() {
 
-	private void sort (){
-
-	}
+    }
 }
--- src/geometric/Rectangle.java
+++ src/geometric/Rectangle.java
@@ -1,46 +1,46 @@
 package geometric;
 
 public class Rectangle implements Geometric {
-	private double x;
-	private double y;
-	private final double width;
-	private final double height;
+    private double x;
+    private double y;
+    private final double width;
+    private final double height;
 
-	public Rectangle(double x, double y, double width, double height) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-	}
+    public Rectangle(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 
-	@Override
-	public double topBorder() {
-		return y + height;
-	}
+    @Override
+    public double topBorder() {
+        return y + height;
+    }
 
-	@Override
-	public double rightBorder() {
-		return x + width;
-	}
+    @Override
+    public double rightBorder() {
+        return x + width;
+    }
 
-	@Override
-	public double bottomBorder() {
-		return y;
-	}
+    @Override
+    public double bottomBorder() {
+        return y;
+    }
 
-	@Override
-	public double leftBorder() {
-		return x;
-	}
+    @Override
+    public double leftBorder() {
+        return x;
+    }
 
-	@Override
-	public double area() {
-		return width * height;
-	}
+    @Override
+    public double area() {
+        return width * height;
+    }
 
-	@Override
-	public void move(double dx, double dy) {
-		x += dx;
-		y += dy;
-	}
+    @Override
+    public void move(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
 }
