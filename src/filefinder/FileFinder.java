bfc96ab42f2d0dfc7b19d7097d90f861b6c790b2
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2022-06-11 15:16:43 +0200

    Completed the file finder part

--- src/filefinder/FileFinder.java
+++ src/filefinder/FileFinder.java
@@ -3,11 +3,20 @@ package filefinder;
 import java.io.File;
 import java.io.IOException;
 
-public class FileFinder {
+public class FileFinder implements Runnable {
 	private final File rootDir;
+	private String filename;
 
 	public FileFinder(String root) throws IOException {
 		rootDir = new File(root);
+		if (!(rootDir.exists() && rootDir.isDirectory())) {
+			throw new IOException(root + " is not a directory");
+		}
+	}
+
+	public FileFinder(String root, String filename) throws IOException {
+		rootDir = new File(root);
+		this.filename = filename;
 		if (!(rootDir.exists() && rootDir.isDirectory())) {
 			throw new IOException(root + " is not a directory");
 		}
@@ -27,6 +36,30 @@ public class FileFinder {
 					find(file, fileName);
 				}
 			}
+		}
+	}
+
+	private void findParallel(File rootDir, String filename) throws IOException {
+		File[] files = rootDir.listFiles();
+		if (files != null) {
+			for (File file : files) {
+				if (file.getName().equals(filename)) {
+					System.out.println("Found at: " + file.getAbsolutePath());
+				} else if (file.isDirectory()) {
+					FileFinder ff = new FileFinder(file.toString(), filename);
+					Thread thread = new Thread(ff);
+					thread.start();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			findParallel(rootDir, filename);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
--- src/filefinder/Main.java
+++ src/filefinder/Main.java
@@ -7,9 +7,26 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			String goal = "needle.txt";
-			String root = "haystack";
+			String root = "../../../ru/haystack";
+
 			FileFinder ff = new FileFinder(root);
+			long timeBefore = System.currentTimeMillis();
 			ff.findFile(goal);
+			long timeAfter = System.currentTimeMillis();
+			long timeItTook = timeAfter - timeBefore;
+			System.out.println("Parallel Run: " + timeItTook + " ms");
+
+			FileFinder ff2 = new FileFinder(root, goal);
+			Thread thread = new Thread(ff2);
+			long timeBefore2 = System.currentTimeMillis();
+			thread.start();
+			long timeAfter2 = System.currentTimeMillis();
+			long timeItTook2 = timeAfter2 - timeBefore2;
+			System.out.println("Sequential Run: " + timeItTook2 + " ms");
+
+			// Parallel Run: 100 ms
+			// Sequential Run: most of the time 0 ms, sometimes between 1 and 4 ms
+			// Amount of cores: 8
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
